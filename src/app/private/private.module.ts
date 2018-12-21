828c1091100c8fd4daf913d6574707517204c4ac
RahmaBasly <35639830+RahmaBasly@users.noreply.github.com>
2018-12-21 19:54:20 +0100

    test

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './layout/layout.component';
 import {RouteModuleRoutingModule} from '../route-module/route-module-routing.module';
 import {MandateService} from './mandate/Services/mandate.service';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AddAuthenticationHeaderInterceptor} from './shared/add-authentication-header-interceptor';
 import {MandateModule} from './mandate/mandate.module';
-import { JobrequestComponent } from './jobrequest/jobrequest.component';
-import { TestCategoryComponent } from './test-category/test-category.component';
-import { FormsModule , ReactiveFormsModule} from '@angular/forms';
-import { ModulesComponent } from './modules/modules.component';
-import { QuestionComponent } from './question/question.component';
+import {JobrequestComponent} from './jobrequest/jobrequest.component';
+import {TestCategoryComponent} from './test-category/test-category.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ModulesComponent} from './modules/modules.component';
+import {QuestionComponent} from './question/question.component';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { ChoicesComponent } from './question/choices/choices.component';
-import { ListJobrequestComponent } from './list-jobrequest/list-jobrequest.component';
-import { TestComponent } from './test/test.component';
+import {ChoicesComponent} from './question/choices/choices.component';
+import {ListJobrequestComponent} from './list-jobrequest/list-jobrequest.component';
+import {TestComponent} from './test/test.component';
+import {ProjectComponent} from './project/project.component';
 
 
 @NgModule({
@@ -26,11 +27,12 @@ import { TestComponent } from './test/test.component';
     MandateModule,
     NgxPaginationModule
   ],
-  declarations: [LayoutComponent, JobrequestComponent, TestCategoryComponent, ModulesComponent, QuestionComponent, ChoicesComponent, ListJobrequestComponent, TestComponent],
+  declarations: [LayoutComponent, JobrequestComponent, TestCategoryComponent, ModulesComponent, QuestionComponent, ChoicesComponent, ListJobrequestComponent, TestComponent, ProjectComponent],
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AddAuthenticationHeaderInterceptor,
     multi: true
   }, MandateService]
 })
-export class PrivateModule { }
+export class PrivateModule {
+}
