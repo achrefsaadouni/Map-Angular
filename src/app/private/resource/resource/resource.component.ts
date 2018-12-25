08f282cd9f54864c0c2cb9b6c2250114a3365d4b
Karim0210 <36571315+Karim0210@users.noreply.github.com>
2018-12-25 02:28:32 +0100

    te

---
+++ src/app/private/resource/resource/resource.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ResourceService} from "./service/resource.service";
+import {Resource} from "../../Models/Resource";
+
+@Component({
+  selector: 'app-resource',
+  templateUrl: './resource.component.html',
+  styleUrls: ['./resource.component.css'],
+    providers:[ResourceService]
+})
+export class ResourceComponent implements OnInit {
+    resourceNoArchived:Resource[];
+
+    constructor(private rs : ResourceService) { }
+
+    ngOnInit() {
+        this.rs.GetAllResourcesNoArchived().subscribe(data =>{this.resourceNoArchived = data});
+    }
+
+
+}
